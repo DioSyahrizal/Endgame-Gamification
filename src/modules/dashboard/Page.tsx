21c17cb2f2928dab4d6fa9d8f73078e5541b539e
diosyahrizal <diosyahrizal@gmail.com>
2019-12-04 09:49:39 +0700

    feat(dashboard): add button hell yeah

--- src/modules/dashboard/Page.tsx
+++ src/modules/dashboard/Page.tsx
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import { Dashboard } from "@kata-kit/dashboard";
+import { Button } from "semantic-ui-react";
 
 export default class Dashboardmain extends Component {
   render() {
-    return <Dashboard title="Dashboard">Halo halo</Dashboard>;
+    return (
+      <Dashboard title="Dashboard">
+        Halo halo <Button>Click Meh!</Button>
+      </Dashboard>
+    );
   }
 }
